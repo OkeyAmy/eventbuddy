8bb662497d78dc63e2513938b058ad07efa9b9d7
Okey Amy <amaobiokeoma@gmail.com>
2025-09-25 16:51:39 +0100

    Update

--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,13 @@ export default defineConfig({
   server: {
     host: "::",
     port: 8080,
+    proxy: {
+      "/api": {
+        target: "http://localhost:3000",
+        changeOrigin: true,
+        rewrite: (path) => path,
+      },
+    },
   },
   plugins: [react(), componentTagger()],
   resolve: {
